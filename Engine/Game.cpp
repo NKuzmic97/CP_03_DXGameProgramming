af17423ef695598ed7d53a42ff4bf5e93b6128da
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-11-04 00:09:46 +0100

    Episode 6 - Challenge B - Find bug using debugger

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -20,7 +20,7 @@
  ******************************************************************************************/
 #include "MainWindow.h"
 #include "Game.h"
-
+//
 Game::Game(MainWindow& wnd)
 	:
 	wnd(wnd),
